626d3ce49185aa01e25089ea7547a28ab6995f35
Martin Neiiendam <fracklen@gmail.com>
2013-09-22 01:19:26 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GreatestNPerGroupTestapp::Application.config.secret_token = '4385224b28d479cd3471322c3283e1a29cef911709e248db20147b6eb3d8af72992f04e8a581ff5ff3fc23dfd4df49db72ff2ac968f207354a56aaa9a147f7ba'
